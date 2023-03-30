55dc6252fd33126c2f184a339dba5dd60ee4cff7
gokcesahinnn <111432634+gokcesahinnn@users.noreply.github.com>
2023-03-30 16:19:50 +0300

    delete

--- webinar/src/test/java/helpers/randomDataGenerator.js
+++
@@ -1,5 +0,0 @@
-function generateRandomName() {
-  const names = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Henry", "Isabella", "James", "Kate", "Liam", "Mia", "Nora", "Oliver", "Penelope", "Quentin", "Riley", "Samantha", "Thomas", "Ursula", "Victoria", "William", "Xander", "Yara", "Zachary"];
-  const randomIndex = Math.floor(Math.random() * names.length);
-  return names[randomIndex];
-}
